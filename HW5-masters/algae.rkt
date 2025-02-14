d13ac4fc7768aa001a550aba5482d13537be19c2
Manas Aggrawal <manasaggrawal@Manass-MacBook-Air.local>
2025-02-14 03:27:59 -0500

    hw5 part4 till func lookup

--- HW5-masters/algae.rkt
+++ HW5-masters/algae.rkt
@@ -1,11 +1,8 @@
 #lang pl 05
 
 #| BNF for the ALGAE language:
-<<<<<<< HEAD
      <PROGRAM> ::= { program <FUN> ... }
      <FUN>     ::= { fun <id> { <id> } <ALGAE> }
-=======
->>>>>>> main
      <ALGAE> ::= <num>
                | { + <ALGAE> ... }
                | { * <ALGAE> ... }
