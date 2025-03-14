fffcfaa3add6e19be91480d31f2853c1ef23dfe2
Manas Aggrawal <manasaggrawal@Manass-MacBook-Air.local>
2025-03-14 19:05:44 -0400

    added more test cases

--- HW11/eleven.rkt
+++ HW11/eleven.rkt
@@ -19,6 +19,13 @@
         [(zero? n) (ackermann (- m 1) 1)]
         [else      (ackermann (- m 1) (ackermann m (- n 1)))]))
 (test (ackermann 3 3) => 61)
+(test (ackermann 0 0) => 1)  
+(test (ackermann 0 5) => 6)  
+(test (ackermann 1 0) => 2)  
+(test (ackermann 1 1) => 3)
+(test (ackermann 2 2) => 7)
+(test (ackermann 2 5) => 13)
+(test (ackermann 3 0) => 5)  
 
 (rewrite (letfuns ([(f x ...) E] ...) B)
          => (let ([g (Y (lambda (funs)
@@ -34,11 +41,22 @@
 
 (letfuns ([(even? n) (if (= n 0) #t (odd?  (- n 1)))]
           [(odd?  n) (if (= n 0) #f (even? (- n 1)))])
-  (and (even? 0)       ; Test even? base case
-       (not (even? 1)) ; Test even? calls odd? 0 (odd? base case)
-       (odd? 1)        ; Test odd? calls even? 0 (even? base case again)
-       (not (odd? 0)))) ; Test odd? base case
-;; TODO test even and odd functions
+  (and (even? 0)       
+       (not (even? 1)) 
+       (odd? 1)        
+       (not (odd? 0))))
+
+(test (letfuns ([(even? n) (if (= n 0) #t (odd?  (- n 1)))]
+                [(odd?  n) (if (= n 0) #f (even? (- n 1)))])
+        (list (even? 0)       
+              (even? 2)       
+              (even? 4)       
+              (odd? 1)        
+              (odd? 3)        
+              (even? 1)       
+              (odd? 0)        
+              (odd? 2)))      
+      => '(#t #t #t #t #t #f #f #f))
 
 ;; an extended example
 (define scan
@@ -63,5 +81,21 @@
 (test (not (scan "(1232)")))
 (test (not (scan "()(")))
 (test (not (scan "())")))
+(test (scan "") => #t)                   
+(test (scan "()") => #t)                 
+(test (scan "(open)") => #t)             
+(test (scan "123") => #t)                
+(test (scan "123 456") => #t)            
+(test (scan "((()))") => #t)             
+(test (scan "(open (open close) close)") => #t)
+(test (not (scan "(")))                  
+(test (not (scan ")")))                  
+(test (not (scan "123 123")) => #f)            
+(test (not (scan "123 122")))            
+(test (not (scan "openclose")) => #f)          
+(test (not (scan "(open close")))        
+(test (not (scan "123x")) => #f)               
+(test (not (scan "())(")))               
+(test (not (scan "(close open)")) => #f)       
 
 (define minutes-spent 120)
